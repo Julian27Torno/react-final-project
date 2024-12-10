148e7b5c450e9ebae127eb57115b1a4134db83b1
Julian27Torno <torno.juliancarlos@auf.edu.ph>
2024-12-10 20:12:22 +0800

    added a assets folder and changed the url

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,6 @@
+import diminsilImage from '../assets/diminsil.jpg';
+import tornoImage from '../assets/torno.jpg';
+import tubilImage from '../assets/tubil.jpg';
 const About = () => {
     return (
       <div className="container mt-5">
@@ -70,7 +73,7 @@ const About = () => {
             <div className="col-md-4">
               <div className="card shadow-sm">
                 <img
-                  src="https://scontent.fmnl33-5.fna.fbcdn.net/v/t39.30808-1/369865538_6653028361478020_6227007946496683084_n.jpg?stp=dst-jpg_s200x200_tt6&_nc_cat=102&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeHw-YC0jDtPDmISKUHYmbTc4breE8DIZsThut4TwMhmxC6KP3JrMyUZ2ePM3BFbWjs62kceWgIOm8xgBYI-beAN&_nc_ohc=H84T-3mbPf8Q7kNvgEol7II&_nc_zt=24&_nc_ht=scontent.fmnl33-5.fna&_nc_gid=AopIQqGJqk8tCofvJM3O_V6&oh=00_AYDGFyygurqt2ASb3olimKpSF5TYiy60T0m0jb25lxUnLQ&oe=675DF948"
+                  src={diminsilImage}
                   className="card-img-top"
                   alt="Team Member"
                 />
@@ -83,7 +86,7 @@ const About = () => {
             <div className="col-md-4">
               <div className="card shadow-sm">
                 <img
-                  src="https://scontent.fmnl33-2.fna.fbcdn.net/v/t39.30808-6/357707727_6385924128141068_1881381892591779604_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHr4cAfaJMcmuks3UhLAazcWxhV-MG9wAVbGFX4wb3ABZQn1rZoN9dnsysRED8-Be6ONKc2i_g0UZqQSaPzrfbf&_nc_ohc=0s5VpBH8RJgQ7kNvgG4-RxB&_nc_zt=23&_nc_ht=scontent.fmnl33-2.fna&_nc_gid=AIHds6Ll74goJk8C5OD1XrZ&oh=00_AYDnl9BAm-IJHEBLTUpPu0jalTLw6W4ra8kvEKTyn-2c0A&oe=675E0AFC"
+                  src={tornoImage}
                   className="card-img-top"
                   alt="Team Member"
                 />
@@ -96,7 +99,7 @@ const About = () => {
             <div className="col-md-4">
               <div className="card shadow-sm">
                 <img
-                  src="https://scontent.fmnl3-1.fna.fbcdn.net/v/t1.6435-9/154617712_3708294709264875_3031566544765844958_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeG43vk6tf4Qz89xbvI959ZVJyIqPRFDYP4nIio9EUNg_nE7NksdJVgoTZ9PBrFRC0qI_tm5KAbssDouMVpJAG-c&_nc_ohc=2jXjQkMbQkMQ7kNvgGj5m1f&_nc_zt=23&_nc_ht=scontent.fmnl3-1.fna&_nc_gid=AERmjsqvPsh_5EzIYEkTz1w&oh=00_AYBdKNcmLa56xWQR-5SN2P__8YaTQgjhHSKJLZp17Y2IKg&oe=6776985F"
+                  src={tubilImage}
                   className="card-img-top"
                   alt="Team Member"
                 />
